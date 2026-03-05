c2a13686820724beb73f91e8accad04e36e61402
Habibi Med Wassim <habibimedwassim@gmail.com>
2026-03-05 21:15:38 +0100

    Reorganize package installation commands in gaming-meta.sh

--- profiles/gaming-meta.sh
+++ profiles/gaming-meta.sh
@@ -5,22 +5,6 @@ echo "==> Updating package lists..."
 sudo apt update
 
 sudo apt install -y \
-    libgif7           libgif7:i386 \
-    libglfw3          libglfw3:i386 \
-    libgstreamer-plugins-base1.0-0 \
-    libgstreamer-plugins-base1.0-0:i386 \
-    libgtk-3-0t64     libgtk-3-0t64:i386 \
-    libjpeg62-turbo   libjpeg62-turbo:i386 \
-    ocl-icd-libopencl1 \
-    ocl-icd-libopencl1:i386 \
-    libopenal1        libopenal1:i386 \
-    libosmesa6        libosmesa6:i386 \
-    libxslt1.1        libxslt1.1:i386 \
-    libmpg123-0t64    libmpg123-0t64:i386 \
-    libxvidcore4 \
-    libvulkan1        libvulkan1:i386
-
-sudo apt install -y \
     ffmpeg \
     libavcodec-extra \
     libavcodec-extra:i386 \
@@ -43,6 +27,23 @@ sudo apt install -y \
     x265 \
     opus-tools \
     flvmeta
+
+sudo apt install -y \
+    libgif7           libgif7:i386 \
+    libglfw3          libglfw3:i386 \
+    libgstreamer-plugins-base1.0-0 \
+    libgstreamer-plugins-base1.0-0:i386 \
+    libgtk-3-0t64     libgtk-3-0t64:i386 \
+    libjpeg62-turbo   libjpeg62-turbo:i386 \
+    ocl-icd-libopencl1 \
+    ocl-icd-libopencl1:i386 \
+    libopenal1        libopenal1:i386 \
+    libosmesa6        libosmesa6:i386 \
+    libxslt1.1        libxslt1.1:i386 \
+    libmpg123-0t64    libmpg123-0t64:i386 \
+    libxvidcore4 \
+    libvulkan1        libvulkan1:i386 \
+    libvulkan-dev     libvulkan-dev:i386
 
 sudo apt install -y \
     timidity \
