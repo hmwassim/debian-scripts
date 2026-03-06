e0aa99a95d046bc6929cc6977c372b2f42c111a1
Habibi Med Wassim <habibimedwassim@gmail.com>
2026-03-07 00:40:49 +0100

    Clean up gaming-meta.sh by removing unused packages
    Removed unnecessary libraries from installation list.

--- profiles/gaming-meta.sh
+++ profiles/gaming-meta.sh
@@ -31,18 +31,7 @@ sudo apt install -t trixie-backports -y \
 sudo apt install -t trixie-backports -y \
     libgif7           libgif7:i386 \
     libglfw3          libglfw3:i386 \
-    libgstreamer-plugins-base1.0-0 \
-    libgstreamer-plugins-base1.0-0:i386 \
-    libgtk-3-0t64     libgtk-3-0t64:i386 \
-    libjpeg62-turbo   libjpeg62-turbo:i386 \
-    ocl-icd-libopencl1 \
-    ocl-icd-libopencl1:i386 \
-    libopenal1        libopenal1:i386 \
     libosmesa6        libosmesa6:i386 \
-    libxslt1.1        libxslt1.1:i386 \
-    libmpg123-0t64    libmpg123-0t64:i386 \
-    libxvidcore4 \
-    libvulkan1        libvulkan1:i386 \
     libvulkan-dev     libvulkan-dev:i386
 
 sudo apt install -t trixie-backports -y \
